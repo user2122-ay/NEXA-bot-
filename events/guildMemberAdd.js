59d75edde7019fd5babc51bde3e3eaf032708228
user2122-ay <anthonybatista2010@gmail.com>
2026-05-01 22:39:26 -0500

    Refactor database connection to use pg Pool

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,12 @@
-import pool from "../database.js";
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 export default {
   name: "guildMemberAdd",

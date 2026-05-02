7c4a07a36c8fd9634217ca8163d0839bf2ee5a45
user2122-ay <anthonybatista2010@gmail.com>
2026-05-01 22:36:28 -0500

    Add guildMemberAdd event for welcoming new members

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,25 @@
+import pool from "../database.js";
+
+export default {
+  name: "guildMemberAdd",
+  async execute(member) {
+    try {
+      const res = await pool.query(
+        "SELECT * FROM welcome_config WHERE guild_id = $1",
+        [member.guild.id]
+      );
+
+      if (!res.rows[0]) return;
+
+      const channel = member.guild.channels.cache.get(res.rows[0].channel_id);
+      if (!channel) return;
+
+      await channel.send({
+        content: `👋 Bienvenido ${member} a **${member.guild.name}** 🎉`
+      });
+
+    } catch (error) {
+      console.error("WELCOME ERROR:", error);
+    }
+  }
+};

f6503787d7e45fd7b8e8cdcbdf821905ae76b117
user2122-ay <anthonybatista2010@gmail.com>
2026-05-29 20:53:34 -0500

    Create guildMemberRemove.js

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,96 @@
+import { EmbedBuilder } from "discord.js";
+import Goodbye from "../models/Goodbye.js";
+
+export default {
+
+  name: "guildMemberRemove",
+
+  async execute(member) {
+
+    try {
+
+      const data = await Goodbye.findOne({
+        guildId: member.guild.id
+      });
+
+      if (!data) return;
+
+      const channel =
+        member.guild.channels.cache.get(
+          data.channelId
+        );
+
+      if (!channel) return;
+
+      const texto = data.message
+
+        .replaceAll("{user}", member.user.tag)
+
+        .replaceAll(
+          "{server}",
+          member.guild.name
+        )
+
+        .replaceAll(
+          "{members}",
+          member.guild.memberCount
+        );
+
+      const embed = new EmbedBuilder()
+
+        .setColor(
+          data.color || "#ED4245"
+        )
+
+        .setDescription(texto)
+
+        .setThumbnail(
+          member.user.displayAvatarURL({
+            dynamic: true,
+            size: 4096
+          })
+        )
+
+        .setFooter({
+          text: member.guild.name,
+          iconURL:
+            member.guild.iconURL({
+              dynamic: true
+            }) || null
+        })
+
+        .setTimestamp();
+
+      if (data.icon) {
+
+        embed.setAuthor({
+          name:
+            `Salida de ${member.guild.name}`,
+          iconURL:
+            member.guild.iconURL({
+              dynamic: true
+            }) || null
+        });
+
+      }
+
+      if (data.image) {
+        embed.setImage(data.image);
+      }
+
+      await channel.send({
+        embeds: [embed]
+      });
+
+    } catch (err) {
+
+      console.error(
+        "❌ GOODBYE ERROR:",
+        err
+      );
+
+    }
+
+  }
+
+};
